cd9e9c12c834aa3468589593fd5408acaf84d3d9
whoismidlaj <its.me.midlaj@gmail.com>
2023-11-09 12:20:43 +0530

    changess

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 // import AboutView from '../views/AboutView.vue';
-import WorksView from '../views/WorksView.vue';
+import WorksView from '../views/Worksview.vue';
 import NotFound from '../views/NotFound.vue';
 
 const router = createRouter({
